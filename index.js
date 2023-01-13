2ae822923c0e06613d7385facea644a40f257c6a
Bryan6734 <97413577+Bryan6734@users.noreply.github.com>
2023-01-13 11:26:13 -0500

    Added timer, score, score card, and fixed bugs

--- index.js
+++ index.js
@@ -10,7 +10,12 @@ var image = document.getElementById("face");
 const startGame = () => {
   const yearSelect = document.getElementById("year");
   const selectedYear = yearSelect.options[yearSelect.selectedIndex].value;
+
+  const timeSelect = document.getElementById("time");
+  const selectedTime = timeSelect.options[timeSelect.selectedIndex].value;
+
   sessionStorage.setItem("selectedYear", selectedYear);
+  sessionStorage.setItem("selectedTime", selectedTime);
   window.location.href = "quiz.html";
 }
 
@@ -18,7 +23,17 @@ const startGame = () => {
 var studentData;
 var answer;
 
+var correctStudentList = [];
+var incorrectStudentList = [];
+
+var correct = 0;
+var incorrect = 0;
+
+var canClick = true;
+
 const selectedYear = sessionStorage.getItem("selectedYear");
+const selectedTime = sessionStorage.getItem("selectedTime");
+
 fetch(`data/students_${selectedYear}.json`)
   .then(response => response.json())
   .then(data => {
@@ -28,11 +43,65 @@ fetch(`data/students_${selectedYear}.json`)
   .then(() => {
     displayQuestion();
     console.log(answer);
-  });
+  })
+  .then(() => {
+    startTimer(selectedTime);
+  })
 
 
+function startTimer(selectedTime){
+  const time = document.querySelector(".time");
+  let seconds = selectedTime;
 
-  
+  let timer = setInterval(() => {
+    seconds -= 0.1;
+    const remainingSeconds = (seconds % 60).toFixed(1);
+    time.textContent = `Time: ${remainingSeconds < 10 ? '0' : ''}${remainingSeconds}`;
+    if (remainingSeconds <= 0.0) {
+
+        
+        canClick = false;
+        clearInterval(timer);
+        time.textContent = "Time: 00:00"
+
+        let numberCorrect = document.getElementById("num-correct");
+        let numberIncorrect = document.getElementById("num-incorrect");
+
+        numberCorrect.innerHTML = correct;
+        numberIncorrect.innerHTML = incorrect;
+
+        let scoreCard = document.getElementById("score-card");
+        scoreCard.classList.toggle("hidden");
+
+        let correctList = document.getElementById("correct-student-list");
+        let incorrectList = document.getElementById("incorrect-student-list");
+
+        for (var i = 0; i < correctStudentList.length; i++) {
+          var item = document.createElement('li');
+          item.appendChild(document.createTextNode(correctStudentList[i].name));
+
+          var itemPic = new Image();
+          itemPic.src = correctStudentList[i].image_path;
+          
+          correctList.appendChild(item);
+          correctList.appendChild(itemPic);
+        }
+
+        for (var i = 0; i < incorrectStudentList.length; i++) {
+          var item = document.createElement('li');
+          item.appendChild(document.createTextNode(incorrectStudentList[i].name));
+
+          var itemPic = new Image();
+          itemPic.src = incorrectStudentList[i].image_path;
+
+          incorrectList.appendChild(item);
+          incorrectList.appendChild(itemPic);
+        }
+    }
+}, 100);
+
+}
+
 function selectRandomFour(){
   const shuffled = shuffle(studentData.slice());
   const randomFour = shuffled.slice(0,4);
@@ -41,7 +110,7 @@ function selectRandomFour(){
 
 function displayQuestion() {
   let currentQuestion = selectRandomFour();
-  answer = currentQuestion[0].name;
+  student = currentQuestion[0];
   image.src = currentQuestion[0].image_path;
   currentQuestion = shuffle(currentQuestion);
   
@@ -59,39 +128,60 @@ function shuffle(array) {
   return array;
 }
 
+function displayScore(){
+  var correctText = document.getElementById("correct-score");
+  var incorrectText = document.getElementById("incorrect-score"); 
 
+  correctText.innerHTML = correct;
+  incorrectText.innerHTML = incorrect;
+
+}
+
+function handleButtonAnswer(button){
+  
+
+
+
+  if (button.innerText == student.name){
+    correct += 1;
+    correctStudentList.push(student);
+  } else {
+    incorrect += 1;
+    incorrectStudentList.push(student);
+  }
+  console.log(correctStudentList)
+  console.log(incorrectStudentList)
+}
 
 buttonA.addEventListener("click", function(){
-  if (buttonA.innerText == answer){
-    displayQuestion()
-  } else {
-    alert("incorrect!")
-  }
+  if (!canClick) return;
+  handleButtonAnswer(buttonA)
+  displayQuestion()
+  displayScore()
 });
 
 buttonB.addEventListener("click", function(){
-  if (buttonB.innerText == answer){
-    displayQuestion()
-  } else {
-    alert("incorrect!")
-  }
+  if (!canClick) return;
+  handleButtonAnswer(buttonB)
+  displayQuestion()
+  displayScore()
 });
 
 buttonC.addEventListener("click", function(){
-  if (buttonC.innerText == answer){
-    displayQuestion()
-  } else {
-    alert("incorrect!")
-  }
+  if (!canClick) return;
+  handleButtonAnswer(buttonC)
+  displayQuestion()
+  displayScore()
 });
 
 buttonD.addEventListener("click", function(){
-  if (buttonD.innerText == answer){
-    displayQuestion()
-  } else {
-    alert("incorrect!")
-  }
+  if (!canClick) return;
+  handleButtonAnswer(buttonD)
+  displayQuestion()
+  displayScore()
 });
+
+
 
 
 
